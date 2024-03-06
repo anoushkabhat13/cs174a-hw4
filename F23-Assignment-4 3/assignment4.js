a6de86003a3665cfc0075b719e4ce6cdbcc620a5
anoushkabhat13 <anoushkabhat2016@gmail.com>
2024-03-06 11:50:08 -0800

    first commit

---
+++ F23-Assignment-4 3/assignment4.js
@@ -0,0 +1,160 @@
+import {defs, tiny} from './examples/common.js';
+
+const {
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
+} = tiny;
+
+const {Cube, Axis_Arrows, Textured_Phong} = defs
+
+export class Assignment4 extends Scene {
+    /**
+     *  **Base_scene** is a Scene that can be added to any display canvas.
+     *  Setup the shapes, materials, camera, and lighting here.
+     */
+    constructor() {
+        // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
+        super();
+
+        // TODO:  Create two cubes, including one with the default texture coordinates (from 0 to 1), and one with the modified
+        //        texture coordinates as required for cube #2.  You can either do this by modifying the cube code or by modifying
+        //        a cube instance's texture_coords after it is already created.
+        this.shapes = {
+            box_1: new Cube(),
+            box_2: new Cube(),
+            axis: new Axis_Arrows()
+        }
+        console.log(this.shapes.box_1.arrays.texture_coord)
+
+
+        // TODO:  Create the materials required to texture both cubes with the correct images and settings.
+        //        Make each Material from the correct shader.  Phong_Shader will work initially, but when
+        //        you get to requirements 6 and 7 you will need different ones.
+        this.materials = {
+            phong: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+            }),
+            texture: new Material(new Textured_Phong(), {
+                //color: hex_color("#ffffff"),
+                ambient: 1, diffusivity: 0.1, specularity: 0.1,
+                ambient_color: hex_color("#000000"),
+                texture: new Texture("assets/stars.png", "NEAREST")
+            }),
+            texture2: new Material(new Textured_Phong(), {
+                //color: hex_color("#ffffff"),
+                ambient: 1, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/earth.gif", "LINEAR_MIPMAP_LINEAR")
+            }),
+        }
+
+
+        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+        this.cube1_transform = Mat4.identity().times(Mat4.translation(-2, 0, 0, 0));
+        this.cube2_transform = Mat4.identity().times(Mat4.translation(2, 0, 0, 0));
+    }
+
+    make_control_panel() {
+        // TODO:  Implement requirement #5 using a key_triggered_button that responds to the 'c' key.
+        this.key_triggered_button("Start/Stop Rotation", ["c"], () =>
+            this.isRotating = !this.isRotating);
+    }
+
+    display(context, program_state) {
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(Mat4.translation(0, 0, -8));
+        }
+
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, 1, 100);
+
+        const light_position = vec4(10, 10, 10, 1);
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+
+
+
+        // TODO:  Draw the required boxes. Also update their stored matrices.
+        //let box_transform = Mat4.scale(2,2,2).times(model_transform);
+        // this.shapes.box_1.draw(context, program_state, box_transform, this.materials.texture);
+
+        this.shapes.box_2.arrays.texture_coord.forEach(
+            (v, i, l) => v[1] = v[1] *2
+        )
+
+        let t = program_state.animation_time / 1000;
+        let dt = program_state.animation_delta_time / 1000;
+        let model_transform = Mat4.identity();
+        let cube1_transform = Mat4.translation(-2,0,0);
+        let cube2_transform = Mat4.translation(2,0,0)
+        // If rotation is toggled on --> rotate both cubes
+        let stop_time = 0;
+        let just_rotated = false;
+
+        if (this.isRotating) {
+            let theta1 = ((Math.PI / 3)) * dt;
+            this.cube1_transform = this.cube1_transform.times(
+                Mat4.rotation(theta1, 1, 0, 0)
+            );
+
+            let theta2 = ((Math.PI/2)) * dt;
+            this.cube2_transform = this.cube2_transform.times(
+                Mat4.rotation(theta2, 0, 1, 0)
+            );
+        }
+
+
+
+
+
+
+
+        this.shapes.box_1.draw(context, program_state, this.cube1_transform, this.materials.texture);
+        this.shapes.box_2.draw(context, program_state, this.cube2_transform, this.materials.texture2);
+        // You can remove the following line.
+        //this.shapes.axis.draw(context, program_state, model_transform, this.materials.phong.override({color: hex_color("#ffff00")}));
+    }
+}
+
+
+class Texture_Scroll_X extends Textured_Phong {
+    // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #6.
+    fragment_glsl_code() {
+        return this.shared_glsl_code() + `
+            varying vec2 f_tex_coord;
+            uniform sampler2D texture;
+            uniform float animation_time;
+            
+            void main(){
+                // Sample the texture image in the correct place:
+                vec4 tex_color = texture2D( texture, f_tex_coord);
+                
+                if( tex_color.w < .01 ) discard;
+                                                                         // Compute an initial (ambient) color:
+                gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
+                                                                         // Compute the final color with contributions from lights:
+                gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
+        } `;
+    }
+}
+
+
+class Texture_Rotate extends Textured_Phong {
+    // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #7.
+    fragment_glsl_code() {
+        return this.shared_glsl_code() + `
+            varying vec2 f_tex_coord;
+            uniform sampler2D texture;
+            uniform float animation_time;
+            void main(){
+                // Sample the texture image in the correct place:
+                vec4 tex_color = texture2D( texture, f_tex_coord );
+                
+                if( tex_color.w < .01 ) discard;
+                                                                         // Compute an initial (ambient) color:
+                gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
+                                                                         // Compute the final color with contributions from lights:
+                gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
+        } `;
+    }
+}
+
